b7fec1bcea33cb502dd63a5e32276105cb5acb0d
Aidan Timson <aidan@timmo.dev>
2026-04-15 11:32:30 +0100

    Cleanup

--- src/utils/info.ts
+++ src/utils/info.ts
@@ -1,6 +1,3 @@
-import { HassEntity } from "home-assistant-js-websocket";
-import { getEntityPicture } from "../ha";
-
 export const INFOS = [
   "name",
   "state",
@@ -12,7 +9,3 @@ export type Info = (typeof INFOS)[number];
 
 export const ICON_TYPES = ["icon", "entity-picture", "none"] as const;
 export type IconType = (typeof ICON_TYPES)[number];
-
-export function computeEntityPicture(stateObj: HassEntity, iconType: IconType) {
-  return iconType === "entity-picture" ? getEntityPicture(stateObj) : undefined;
-}
